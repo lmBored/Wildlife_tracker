71123479ee42c4d0da04b9d36f6f6790dadc39ae
lmBored <baokhoi136@gmail.com>
2024-11-16 15:32:03 +0100

    adff

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,50 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-const MapScreen = () => {
-  const [location, setLocation] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map}>
-        {location && (
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="You are here"
-            pinColor="red"
-          />
-        )}
-      </MapView>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-export default MapScreen;
+export default function App() {
+	return (
+		<View style={{ flex: 1 }}>
+			<MapView style={StyleSheet.absoluteFill} />
+		</View>
+	);
+}
